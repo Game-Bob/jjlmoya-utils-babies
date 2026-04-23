f71583ffde2d3a1121d58020fe735396bf73a947
jjlmoya <juanjo1989@gmail.com>
2026-04-23 22:24:12 +0200

    fix: bibliography

--- src/tool/baby-budget-planner/bibliography.ts
+++ src/tool/baby-budget-planner/bibliography.ts
@@ -1,14 +1,14 @@
 export const bibliography = [
   {
     name: 'Financial Planning for New Parents - Investopedia',
-    url: 'https://www.investopedia.com/articles/pf/08/financial-planning-baby.asp',
+    url: 'https://www.investopedia.com/heres-how-to-give-your-new-baby-an-unbeatable-financial-edge-11729830',
   },
   {
     name: 'Average Cost of Raising a Child - USDA Report',
-    url: 'https://www.usda.gov/media/blog/2017/01/13/cost-raising-child',
+    url: 'https://www.usda.gov/about-usda/news/blog/cost-raising-child',
   },
   {
-    name: 'Baby Cost Calculator - BabyCenter',
-    url: 'https://www.babycenter.com/baby-cost-calculator',
+    name: 'How much extra did you spend per month in your first year - Reddit - UKParenting',
+    url: 'https://www.reddit.com/r/UKParenting/comments/11ciwee/how_much_extra_did_you_spend_per_month_in_your/?tl=es-es',
   },
 ];
